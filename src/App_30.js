c1e3e03c395c27ab57c803b235ba6493dadd83b4
Allen11116 <allen1116huang@gmail.com>
2021-09-30 19:57:56 +0800

    first commit

---
+++ src/App_30.js
@@ -0,0 +1,15 @@
+import Todo_30 from'./components/Todo_30'
+import './App.css';
+
+function App_30() {
+  return (
+    <div>
+    <h1>My Todos</h1>
+    <Todo_30 text="Leaarn React"></Todo_30>
+    <Todo_30 text="Leaarn SQK"></Todo_30>
+    <Todo_30 text="Leaarn NextJS"></Todo_30>
+    </div>
+  );
+}
+
+export default App_30;
